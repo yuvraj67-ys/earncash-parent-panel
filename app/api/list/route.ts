c94c33b22632dc545a3cbd894ea566f704962a8f
yuvraj67-ys <ys493587@gmail.com>
2026-02-27 08:19:14 +0530

    Delete app/api directory

--- app/api/list/route.ts
+++
@@ -1,7 +0,0 @@
-import { kv } from '@vercel/kv';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const screenshots = await kv.get<any[]>('screenshots') || [];
-  return NextResponse.json(screenshots);
-}
